d1353839acff254c56808c53d79f6b85e9d28a54
Linkoln195 <gilioko195@gmail.com>
2023-10-29 15:36:41 +0100

    add RulesPage

--- client/src/pages/Rules/Rulespage.jsx
+++ client/src/pages/Rules/Rulespage.jsx
@@ -1,9 +1,26 @@
+import { useEffect, useState } from 'react'
+import Header from '../../components/Rules/Header/Header'
+import Main from '../../components/Rules/Main/Main'
 import classes from './Rules.module.css'
 
 function Rules() {
+	const [scroll, setScroll] = useState(0)
+
+	console.log(`scroll = ${scroll}`)
+
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll)
+		return () => window.removeEventListener('scroll', handleScroll)
+	}, [])
+
+	const handleScroll = () => {
+		setScroll(window.scrollY)
+	}
+
 	return (
-		<div className={classes.rules_page}>
-			<p>Rulespage</p>
+		<div className={classes.rules__page}>
+			<Header scroll={scroll} />
+			<Main scroll={scroll} />
 		</div>
 	)
 }
