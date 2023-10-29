79b122efe05db2b4e1a2b5ffb2ef859044a23bcc
Linkoln195 <gilioko195@gmail.com>
2023-10-29 16:37:38 +0100

    completed site, add ContactsPage

---
+++ client/src/components/Contacts/Main/Main.jsx
@@ -0,0 +1,66 @@
+import secondIcon from '../../../assets/style/Images/contacts_email_icon.png'
+import firstIcon from '../../../assets/style/Images/contacts_phone_icon.png'
+import thirdIcon from '../../../assets/style/Images/contacts_time_icon.png'
+import classes from './Main.module.css'
+
+function Main({ scroll }) {
+	return (
+		<main
+			className={scroll > 250 ? classes.main__page_fixed : classes.main__page}
+		>
+			<section className={classes.main__section}>
+				<div className={classes.main__container}>
+					<div className={classes.main__container}>
+						<div className={classes.main__heading}>
+							<h1 className={classes.main__heading_title}>Контакты</h1>
+						</div>
+					</div>
+
+					<div className={classes.main__contacts}>
+						<div className={classes.contacts__container}>
+							<ul className={classes.contacts__list}>
+								<li className={classes.contacts__item}>
+									<img
+										src={firstIcon}
+										className={classes.contacts__icon_phone}
+									></img>
+									<a className={classes.contacts__link} href='tel:+74950152662'>
+										+7 (495) 015-26-62
+									</a>
+								</li>
+								<li className={classes.contacts__item}>
+									<img
+										src={secondIcon}
+										className={classes.contacts__icon_email}
+									></img>
+									<a
+										className={classes.contacts__link}
+										href='mailto:info@cyberloga.ru'
+									>
+										info@cyberloga.ru
+									</a>
+								</li>
+								<li className={classes.contacts__item}>
+									<img
+										src={thirdIcon}
+										className={classes.contacts__icon_time}
+									></img>
+									<span className={classes.contacts__text}>
+										Пн. - Вс.: круглосуточно
+									</span>
+								</li>
+							</ul>
+							<p className={classes.content__info}>
+								По предложениям и вопросам о сотрудничестве в области маркетинга
+								и PR пишите на{' '}
+								<a href='mailto:gilioko195@gmail.com'>gilioko195@gmail.com</a>
+							</p>
+						</div>
+					</div>
+				</div>
+			</section>
+		</main>
+	)
+}
+
+export default Main
--- client/src/pages/Contacts/Contactspage.jsx
+++ client/src/pages/Contacts/Contactspage.jsx
@@ -1,9 +1,26 @@
+import { useEffect, useState } from 'react'
+import Header from '../../components/Contacts/Header/Header'
+import Main from '../../components/Contacts/Main/Main'
 import classes from './Contacts.module.css'
 
 function Contacts() {
+	const [scroll, setScroll] = useState(0)
+
+	console.log(`scroll = ${scroll}`)
+
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll)
+		return () => window.removeEventListener('scroll', handleScroll)
+	}, [])
+
+	const handleScroll = () => {
+		setScroll(window.scrollY)
+	}
+
 	return (
-		<div className={classes.contacts_page}>
-			<p>Contactspage</p>
+		<div className={classes.contacts__page}>
+			<Header scroll={scroll} />
+			<Main scroll={scroll} />
 		</div>
 	)
 }
